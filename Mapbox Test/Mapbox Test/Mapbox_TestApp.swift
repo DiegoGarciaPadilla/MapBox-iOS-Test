1f85ede4e7d5d779a610633139dd0efb5a38ae64
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-09-28 16:16:35 -0600

    Create XCode project

---
+++ Mapbox Test/Mapbox Test/Mapbox_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapbox_TestApp.swift
+//  Mapbox Test
+//
+//  Created by Diego Antonio García Padilla on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Mapbox_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mapbox Test/Mapbox TestTests/Mapbox_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mapbox_TestTests.swift
+//  Mapbox TestTests
+//
+//  Created by Diego Antonio García Padilla on 28/09/24.
+//
+
+import Testing
+@testable import Mapbox_Test
+
+struct Mapbox_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
